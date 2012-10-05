c3954f05805ca6acd690a5caa5bf39dad6128426
Daniel Morrison <daniel@collectiveidea.com>
2012-10-05 10:54:13 -0400

    Fix a broken controller test

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, :username => 'danielmorrison'
     assert_response :success
   end
 
